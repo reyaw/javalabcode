4a5f4cf1a90368e3fecf919b1c785eb257656688
reyaw <reyaw@protonmail.com>
2021-09-23 17:10:10 -0400

    day 3 lab 2

--- code/3_2_Looping/lab-code/src/main/java/com.codingtemple/Looping.java
+++ code/3_2_Looping/lab-code/src/main/java/com.codingtemple/Looping.java
@@ -9,5 +9,22 @@ public class Looping {
       * Within the loop, print out the value of your counter.
       *
       * */
+      for(int i = 0; i < 10; i++){
+         System.out.println(i);
+      }
+
+      int j = 0;
+      while(j < 10){
+         System.out.println(j);
+         j++;
+      }
+
+      int k = 0;
+      do {
+         System.out.println(k);
+         k++;
+      }
+      while (k < 10);
+
    }
 }
